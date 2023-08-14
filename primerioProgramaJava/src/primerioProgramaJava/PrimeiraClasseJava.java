a516c8a3a325dca8c00bada84bb2d1a1e8119633
adminti <adminti@host.docker.internal>
2023-08-14 14:40:43 -0300

    Criação do projeto

---
+++ primerioProgramaJava/src/primerioProgramaJava/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package primerioProgramaJava;
+
+
+public class PrimeiraClasseJava {
+	//Main é um método auto executável em JAVA
+	public static void main(String[] args) {
+		//Escreve texto no console
+		System.out.println("Meu primeiro programa em JAVA");
+		System.out.println("Java é orientado a objetos");
+		
+		
+	}
+}
